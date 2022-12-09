b9140e335a98883578501f02f98c33b23b8b34fc
Niklas Buling <nbuling@phoenixcontact.com>
2022-12-09 12:28:37 +0100

    Day 7 progress

--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -3,6 +3,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] input = File.ReadLines("input.txt").ToArray();
+        Dictionary<string,int> directorysize = new Dictionary<string, int>();
+        List<string> Path = new List<string>();
+        foreach(var item in input){
+
+            if(item.Contains("cd ")){
+                string folder = item.Split(" ")[2];
+                Path.Add(folder);
+            }
+        }
+        foreach(string item in Path.GetRange(0,5)){
+        Console.WriteLine(item);
+        }
     }
 }
